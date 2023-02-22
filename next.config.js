a514869c7f7bd2090b698aa015dd5e3cdc9760c0
Binyamin Galinsky <binygal@gmail.com>
2023-02-22 21:16:12 +0200

    hermione: fix to proper use of withPWA.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
-  dest: 'public',
   reactStrictMode: true,
   swcMinify: true,
 })
